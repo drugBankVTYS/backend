451d5b56f4b22b94d1008b69fc3cfc04ca6d2720
Muhammed Samet Akgul <msametakgul@gmail.com>
2023-11-14 14:31:10 +0300

    feat: Create functions for HTTP requests

--- backend/routes/DrugRoutes.js
+++ backend/routes/DrugRoutes.js
@@ -3,11 +3,22 @@ const drugController= require('../controller/DrugsContoller');
 const router = express.Router();
 
 
+//api/alldrugs  -> Get all drugs
+router.get('/alldrugs',drugController.allDrugs);
 
-//api/singledrug/:id
+//api/singledrug/:id   -> Get only 1 drug
 router.get('/singledrug/:id' , drugController.getSingleDrug);
-//api/createdrug
+
+//api/createdrug   ->Create a new drug
 router.post('/createdrug', drugController.createDrug);
 
+//api/deletedrug/:id   -> Delete a drug
+router.delete('/deletedrug/:id' , drugController.deleteDrug);
+
+//api/updatedrug/:drug_id  -> Update drug
+router.put('/updatedrug/:drug_id' ,drugController.updateDrug);
+
+//api/show?name=  -> search based on name 
+router.get('/showdrug', drugController.showDrugWithName);
 
 module.exports=router;
